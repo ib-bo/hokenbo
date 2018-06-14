5b1b42f9481179494cd0d415afcbfa587acc9f73
yuto <yuto@yuto-MacBookPro.local>
2018-06-14 21:59:22 +0900

    no message

--- src/conponents/Slick.js
+++ src/conponents/Slick.js
@@ -31,7 +31,7 @@ class Slick extends React.Component {
         adaptiveHeight
         centerPadding='16px'
         draggable
-        cssEase= 'cubic-bezier(0.600, -0.280, 0.735, 0.045)'
+        cssEase= 'cubic-bezier(0.19, 1, 0.22, 1)'
         className={classes.slider}
       >
         {this.props.slides.map(slide => {
